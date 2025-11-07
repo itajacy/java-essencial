898202721914a9f883913dd4700d4fdd733b1e5d
ita Dev <itajacy@gmail.com>
2025-11-06 23:50:53 -0300

    aula 20 - comentarios

---
+++ projeto-essencial/src/fundamentos/Comentarios.java
@@ -0,0 +1,25 @@
+package fundamentos;
+
+// Estamos usando uma classe.  O que é isso?
+
+/*
+ * Classe responsaovel por exibir os tipos de comentarios
+ * disponiceis dentro da linguagem java
+ *
+ * @author Leonardo Leitao
+ */
+
+public class Comentarios {
+
+    /*
+    * teste de varias
+    * linhas de comentario
+    * o método main() é a porta de entrada de uma aplicacao java
+    *
+    * */
+    static void main() {
+        // Exibe o texto no console
+        System.out.println("Comentarios do código");
+
+    }
+}
