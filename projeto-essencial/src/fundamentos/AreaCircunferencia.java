1fd1df6a029a9a7a042724e399277aaa0340ae39
itajacy TI02 <itajacy@bsd.com.br>
2025-11-07 16:35:06 -0300

    Variaveis, e Area Circunferencia

---
+++ projeto-essencial/src/fundamentos/AreaCircunferencia.java
@@ -0,0 +1,27 @@
+package fundamentos;
+
+public class AreaCircunferencia {
+    static void main() {
+        // raio com o valor de 10
+
+        // PI 3,1415 (constante)
+
+        // AREA -> pi * Raio ao Quadrado
+
+        // 3.1415 (double)
+
+        final double pi = 3.1415;
+
+        double raio = 10.0;
+
+        double area =  pi * (raio* raio);
+
+        System.out.println("Area da circunferencia de raio =" + raio +" = " + area);
+    }
+
+
+
+
+
+
+}
---
+++ projeto-essencial/src/fundamentos/VariaveisEConstantes.java
@@ -0,0 +1,27 @@
+package fundamentos;
+
+public class VariaveisEConstantes {
+    static void main() {
+
+        // Váriavel ou constante para armazernar o valor da idade
+        // 1. Declarar uma variável
+        int idade;
+
+        // 2.  Inicializar a vaariáavel
+
+        idade = 3;
+
+        //Aqui temos uma constante (final)
+       final String nome = "Joao da Silva";
+        System.out.println(nome);
+        System.out.println(idade);
+
+
+        int a =4;
+        int b= 7;
+
+        System.out.println(a+b);
+
+
+    }
+}
