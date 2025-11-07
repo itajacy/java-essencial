3145d1f6fde9c7feaf0b3203605dcf87706a49ed
ita Dev <itajacy@gmail.com>
2025-11-06 23:32:57 -0300

    aulas 14 a 19

---
+++ hospital/src/fluxos/AltaPaciente.java
@@ -0,0 +1,4 @@
+package fluxos;
+
+public class AltaPaciente {
+}
---
+++ projeto-essencial/src/fundamentos/PrimeiroExemplo.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class PrimeiroExemplo {
+
+    static void main() {
+        System.out.println("Olá Formação Java");
+        {
+            System.out.println("Olá Formação Java");
+            System.out.println("Olá Formação Java");
+
+        }
+    }
+
+}
