6be3af56c8357a636ea856c8ef6ee25ca21c9098
ita Dev <itajacy@gmail.com>
2025-11-08 11:49:52 -0300

    aula 26 inferencia

---
+++ projeto-essencial/src/fundamentos/Inferencia.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+public class Inferencia {
+    static void main() {
+        var x = 1;
+
+        System.out.println(x);
+
+        var y = false;
+        System.out.println(y);
+
+        var t = "teste";
+        System.out.println(t);
+
+
+    }
+}
