18ca0fc7f58e227394b3cc7078c36fc350014fdb
ita Dev <itajacy@gmail.com>
2025-11-08 16:25:18 -0300

    aula 35 - convertendo tipos numericos #1

---
+++ projeto-essencial/src/fundamentos/ConversaoDeTipos.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class ConversaoDeTipos {
+
+    /*
+    * conversao implicita de um tipo menor para um maior
+    * byte - short - int - long
+    *
+    * conversao explicita de um tipo maior para um menor. exemplo: int para byte
+    *
+    * */
+
+}
