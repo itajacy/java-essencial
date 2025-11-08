3ed8ec6eda66a02525051d664a6f84276fc8c04c
ita Dev <itajacy@gmail.com>
2025-11-08 15:41:41 -0300

    aula 32 - Console saida

---
+++ projeto-essencial/src/fundamentos/ConsoleSaida.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+public class ConsoleSaida {
+    static void main() {
+        System.out.print("Bom");
+        System.out.print("\tdia\n\n");
+
+        System.out.println("Bom");
+        System.out.println("dia");
+
+        System.out.printf("MegaSena: %d %d %d %d %d %d %n", 7, 14,22,37,42,59);
+
+        System.out.printf("Salario: %.1f%n", 8474.769);
+        System.out.print("fim");
+
+    }
+}
