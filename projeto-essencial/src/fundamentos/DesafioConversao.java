d43af12909fb2dc56b8bf581fff086e4d5b17081
ita Dev <itajacy@gmail.com>
2025-11-09 10:29:12 -0300

    aula 39 - Desafio, conversao

---
+++ projeto-essencial/src/fundamentos/DesafioConversao.java
@@ -0,0 +1,28 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class DesafioConversao {
+    static void main(){
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Entre com o primeiro salário: ");
+        String sal1 = teclado.nextLine();
+        sal1 = sal1.replace(",",".");
+        double s1 = Double.parseDouble(sal1);
+        System.out.print("Entre com o segundo salário: ");
+        String sal2 = teclado.nextLine();
+        sal2 = sal2.replace(",",".");
+        double s2 = Double.parseDouble(sal2);
+        System.out.print("Entre com o terceiro salário: ");
+        String sal3 = teclado.nextLine();
+        sal3 = sal3.replace(",",".");
+        double s3 = Double.parseDouble(sal3);
+
+        double mediaSal = (s1+s2+s3)/3;
+        System.out.printf("Media Salarial: %.2f",mediaSal);
+
+
+    }
+}
