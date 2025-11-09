15fc390f036a4a85614529fd50390fc818b30904
ita Dev <itajacy@gmail.com>
2025-11-09 10:33:32 -0300

    aula 39 - Desafio, conversao + melhorado

--- projeto-essencial/src/fundamentos/DesafioConversao.java
+++ projeto-essencial/src/fundamentos/DesafioConversao.java
@@ -8,16 +8,13 @@ public class DesafioConversao {
         Scanner teclado = new Scanner(System.in);
 
         System.out.print("Entre com o primeiro salário: ");
-        String sal1 = teclado.nextLine();
-        sal1 = sal1.replace(",",".");
+        String sal1 = teclado.nextLine().replace(",",".");
         double s1 = Double.parseDouble(sal1);
         System.out.print("Entre com o segundo salário: ");
-        String sal2 = teclado.nextLine();
-        sal2 = sal2.replace(",",".");
+        String sal2 = teclado.nextLine().replace(",",".");
         double s2 = Double.parseDouble(sal2);
         System.out.print("Entre com o terceiro salário: ");
-        String sal3 = teclado.nextLine();
-        sal3 = sal3.replace(",",".");
+        String sal3 = teclado.nextLine().replace(",",".");
         double s3 = Double.parseDouble(sal3);
 
         double mediaSal = (s1+s2+s3)/3;
