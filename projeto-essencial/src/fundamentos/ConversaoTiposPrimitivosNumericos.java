8a8019215a034384e8051c9338754dba7fa13f71
ita Dev <itajacy@gmail.com>
2025-11-09 09:24:04 -0300

    aula 36 - Conversao de tipos numericos

---
+++ projeto-essencial/src/fundamentos/ConversaoTiposPrimitivosNumericos.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+public class ConversaoTiposPrimitivosNumericos {
+    static void main() {
+
+        double a = 1;  //conversão implícita
+        System.out.println(a);
+
+        int b = (int) 1.99; // conversão explícita (CAST)
+        System.out.println(b);
+
+        long c = 123380;
+        byte d = (byte)c;
+        System.out.println(d);
+
+        
+
+
+
+
+
+    }
+}
