10a08a32207d613463aa03dd9fb3fbc69e1608e1
itajacy TI02 <itajacy@bsd.com.br>
2025-11-10 15:25:06 -0300

    aula 46 - operadores relacionais

---
+++ projeto-essencial/src/Operadores/Relacionais.java
@@ -0,0 +1,27 @@
+package Operadores;
+@SuppressWarnings("all")
+public class Relacionais {
+    static void main() {
+
+        int a = 7;
+        int b = 3;
+
+        System.out.println(a >b);
+        System.out.println(b > a);
+        System.out.println(a >= b);
+        System.out.println(b >= a);
+
+        System.out.println( a == 7);
+        System.out.println( a == b);
+
+        System.out.println( a != 7);
+        System.out.println( a != b);
+        System.out.println( 7 == '7'); // Considera o tipo
+//        System.out.println( 7 == "7"); 
+
+
+
+
+
+    }
+}
