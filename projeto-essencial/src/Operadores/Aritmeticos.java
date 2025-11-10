45966869c276b1a50a093e84b31d5aa04bf7b4e8
itajacy TI02 <itajacy@bsd.com.br>
2025-11-10 14:50:13 -0300

    aula 43 - operadores aritmeticos

---
+++ projeto-essencial/src/Operadores/Aritmeticos.java
@@ -0,0 +1,21 @@
+
+static void main() {
+
+    IO.println("Teste");
+
+    int a = 7;
+    int b = 3 ;
+
+    IO.println(a+b);
+    IO.println(a+12);
+    IO.println(14+12+7-1);
+
+    IO.println(a+b);
+    IO.println(a-b);
+    IO.println(a*b);
+    IO.println(a/(double)b);
+    IO.println("Módulo " +(a%b)); // para saber se é para ou ímpar
+
+
+
+}
