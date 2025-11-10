24c635a00f475a079ebcf08860499f04fdd7141b
itajacy TI02 <itajacy@bsd.com.br>
2025-11-10 14:38:28 -0300

    aula 42 - tipos operadores

---
+++ projeto-essencial/src/fundamentos/operadores/Operadores.java
@@ -0,0 +1,37 @@
+package fundamentos.operadores;
+
+
+/*
+*  OPERADORES
+*
+* -UNARIOS
+* a--> operando
+* + - * / --> operador
+* a++
+*
+*
+* -BINARIOS
+* a+b
+* c-7
+*
+* -TERNARIO
+* a ? b : c
+*
+* --------------------------------------------
+* Operadores Aritméticos
+* + - * / %
+*
+* Operadores Relacionais
+* > < >= <= == !=
+*
+* Operadores Lógicos
+*  AND  OR   XOR    NOT
+*
+* Operadores Atribuição
+*  =  += -=  *= /=
+*
+* */
+
+public class Operadores {
+
+}
