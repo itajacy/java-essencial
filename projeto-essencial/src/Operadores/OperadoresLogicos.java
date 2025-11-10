750d35fe1a2f9e151cdaa1fa48c88ce01a7a6b3b
itajacy TI02 <itajacy@bsd.com.br>
2025-11-10 15:57:21 -0300

    aula 48 - operadores logicos - tabela verdade

---
+++ projeto-essencial/src/Operadores/OperadoresLogicos.java
@@ -0,0 +1,47 @@
+void main() {
+}
+    /*
+    * Operadores Logicos
+    * (Tabela verdade)
+    *
+    * tv65 3980
+    * AND(E)  * terca  *  quinta  *  sabado
+    *               V       V           V
+    *               V       F           F
+    *               F       V           F
+    *               F       F           F
+    *
+    *
+    * sorvete
+    * OR(OU)  * terca  *  quinta  *  sabado
+    *               V       V           V
+    *               V       F           V
+    *               F       V           V
+    *               F       F           F
+    *
+    * tv32 1380 (ou exlusivo)
+    * XOR(OU)  * terca  *  quinta  *  sabado
+    *               V       V           F   // pq se terca e quinta derem certo comprarei a tv de 65
+    *               V       F           V
+    *               F       V           V
+    *               F       F           F
+    *
+    *
+    *
+    *
+    *
+    * trabalhos
+    * 1500 terca
+    * 2500 quinta
+    *
+    * tv 65 3980
+    *
+    *
+    * tv32   1380
+    * sorvete
+    *
+    * nao vao comprar nada -> saúde
+    *
+    *
+    *
+    * */
