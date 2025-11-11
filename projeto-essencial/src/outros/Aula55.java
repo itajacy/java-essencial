deaed171a6865f0b2d7c4caf32eea268cda9be94
itajacy TI02 <itajacy@bsd.com.br>
2025-11-11 08:59:46 -0300

    aula 55 - Resumo JAVA - jdk / jre /jvm

---
+++ projeto-essencial/src/outros/Aula55.java
@@ -0,0 +1,34 @@
+package outros;
+
+public class Aula55 {
+
+
+}
+
+/*
+*  Resumo
+*       Arquitetura Java
+*
+* JRE - java runtime environment
+*
+*
+*
+*
+* JDK - java development kit
+*
+*
+*
+*
+* JVM - JAVA VIRTUAL MACHINE (jre / jdk)
+*
+*
+*  *.java  -->  javac  -->  *.class  (bytecode - código compilado) --> interpretado pela JVM
+*
+* Escreva uma vez e execute em qualquer lugar!
+*
+* *.class --> JVM --> (windows, macOs, linux)
+*
+*
+*
+*
+* */
